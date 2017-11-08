f650f015dcce1e2e544f5e5adc05e922d77b6b7f
Casema919 <cmaggio@scu.edu>
2017-11-07 20:17:20 -0800

    Add files via upload

---
+++ PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	// public OVRManager ovr;
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update () {
+		// Debug.Log(OVRInput.Get(OVRInput.Button.One));
+		// Debug.Log(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTrackedRemote));
+		Debug.Log(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger));
+	}
+}
